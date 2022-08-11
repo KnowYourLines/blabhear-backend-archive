06e18e07336c871c79bd35ca6da54641fe054bef
Johnny <knowyourlines@outlook.com>
2022-08-11 23:14:02 +0100

    disconnect and leave channel group

--- recblab/consumers.py
+++ recblab/consumers.py
@@ -7,8 +7,11 @@ class RoomConsumer(AsyncJsonWebsocketConsumer):
 
 class UserConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
-        user_id = self.scope["url_route"]["kwargs"]["user_id"]
+        self.username = str(self.scope["url_route"]["kwargs"]["user_id"])
         user = self.scope["user"]
-        if user_id == user.username:
-            await self.channel_layer.group_add(str(user_id), self.channel_name)
+        if self.username == user.username:
+            await self.channel_layer.group_add(self.username, self.channel_name)
             await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.username, self.channel_name)
