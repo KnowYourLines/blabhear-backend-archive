f6b5a7779d8ea90b894a92074bd1a26b547da196
Johnny <knowyourlines@outlook.com>
2022-08-11 23:09:29 +0100

    only user can connect to their own user consumer

--- recblab/consumers.py
+++ recblab/consumers.py
@@ -6,4 +6,9 @@ class RoomConsumer(AsyncJsonWebsocketConsumer):
 
 
 class UserConsumer(AsyncJsonWebsocketConsumer):
-    pass
+    async def connect(self):
+        user_id = self.scope["url_route"]["kwargs"]["user_id"]
+        user = self.scope["user"]
+        if user_id == user.username:
+            await self.channel_layer.group_add(str(user_id), self.channel_name)
+            await self.accept()
